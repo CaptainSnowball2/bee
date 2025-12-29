93f9c0fc1df796bc4f96326f313f7d78c344050b
CaptainSnowball2 <arno.kruger2013@outlook.com>
2025-12-29 17:50:03 +0100

    Add files via upload

--- Bee game.py
+++ Bee game.py
@@ -10,12 +10,20 @@ B.x=300
 B.y=250
 F.x=R
 F.y=A
+Score=0
+GameOver=False
 def draw():
     screen.blit("background",(0,0))
     B.draw()
     F.draw()
+    screen.draw.text("score: "+str(Score),(515,20))
+    if GameOver==True:
+        screen.fill("black")
+        screen.draw.text("GameOver",(300,250))
+        screen.draw.text("Score: "+str(Score),(300,300))
 
 def update():
+    global Score
     if keyboard.w:
         B.y = B.y - 2
     if keyboard.a:
@@ -24,4 +32,14 @@ def update():
          B.y = B.y + 2      
     if keyboard.d:
          B.x = B.x + 2  
+    if B.colliderect(F):
+        Score+=1
+        F.x=random.randint(55,550)
+        F.y=random.randint(50,450)
+
+def timer():
+    global GameOver
+    GameOver=True
+clock.schedule(timer, 10.0)
+
 pgzrun.go()
